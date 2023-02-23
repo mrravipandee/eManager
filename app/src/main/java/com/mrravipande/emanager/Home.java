7839688c5a3184a6d9865aa36a59cfd9861e5d3a
Ravi Pandey <imravipanday@gmail.com>
2023-02-23 09:05:24 +0530

    Login with Database

---
+++ app/src/main/java/com/mrravipande/emanager/Home.java
@@ -0,0 +1,27 @@
+package com.mrravipande.emanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+
+    }
+}
